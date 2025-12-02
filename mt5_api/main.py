7c426bcbc54c4f45ae60fa44d4889b58549e3b57
wvallone <william.vallone@ags-it.com>
2025-12-02 15:53:41 +0100

    symbols

--- mt5_api/main.py
+++ mt5_api/main.py
@@ -9,8 +9,22 @@ from pydantic import BaseModel
 import sys
 import uvicorn
 from logger import log, logs
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(title="MT5 Local API")
+
+# Permette ad Angular in sviluppo di chiamare questa API
+origins = ["http://localhost:4200", "http://127.0.0.1:4200"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class LoginRequest(BaseModel):
     login: int
